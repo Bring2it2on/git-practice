fc430b6c3c6911e1d46175d05f74a531e0fec458
Bring2it2on <yjkl0516@gmail.com>
2024-05-10 10:56:17 +0900

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        System.out.println("우린 할 수 있다!");
+        
+    }
+}
